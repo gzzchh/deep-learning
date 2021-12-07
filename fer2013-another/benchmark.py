296dae04ba6a27d8b4c53192bfcd59b20c3cd80f
gzzchh <xjdzch@126.com>
2021-12-07 17:22:07 +0800

    还是搞不定该死的绘图

--- fer2013-another/benchmark.py
+++ fer2013-another/benchmark.py
@@ -52,8 +52,8 @@ def trainBenchmark(modelNamePrefix, schedulerChoice, net, times=1):
         fig, ax = plt.subplots()
         ax.plot(epochList, lossRateList, color="blue", label="轮数")
         ax.plot(epochList, learnRateList, color="orange", label="学习率")
-        ax.plot(epochList, trainAccList, color="green", label="训练正确率")
-        ax.plot(epochList, testAccList, color="red", label="测试正确率")
+        # ax.plot(epochList, trainAccList, color="green", label="训练正确率")
+        # ax.plot(epochList, testAccList, color="red", label="测试正确率")
 
         plt.savefig(f"{modelNamePrefix}-{i}.png")
         plt.show()
--- fer2013-another/detect.py
+++ fer2013-another/detect.py
@@ -1,9 +1,14 @@
 import torch
 import DeepEmotionModel
-import cv2
-import matplotlib.pyplot as plt
+
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 net = DeepEmotionModel.DeepEmotionModel()
 # 加载模型
 net.load_state_dict(torch.load("DeepEmotionModel.pk1", map_location=device))
 net.to(device)
+
+import cv2
+import matplotlib.pyplot as plt
+
+frame = cv2.imread('IMG_20190608_171842.jpg')
+plt.imshow(frame, cv2.COLOR_BGR2RGB)
